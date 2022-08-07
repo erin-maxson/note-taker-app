cd9f04769ef56877f1571004b5d639995fbf4145
erin-maxson <erin@cohatch.com>
2022-08-07 14:22:25 -0700

    working on api issue, adding package lock

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,11 @@
 const router=require("express").Router();
 const fs=require("fs");
-const {addNewNote, dbUpdate} = require("../lib/note_info")
+const {addNewNote} = require("../lib/note_info")
 const db=require("../db/db.json");
 
-router.get("/api/notes",(req,res)=>{
-     res.json(db);
-})
-
+router.get("/api/notes", (req, res)=>{
+     res.json("db");
+});
 
 router.post("/api/notes", (req, res)=>{
     console.log(req.body);
@@ -14,13 +13,10 @@ router.post("/api/notes", (req, res)=>{
     res.json(addNote);
 });
 
-    //db is array, think about array function to add new element or value of an array
-    //think about update the revised db with new data into the db.json file
+// router.delete("/api/notes", (req, res)=>{
+//     const params = req.params.id
+//     dbUpdate(params, notes);
+//     res.redirect('');
+// });
 
-router.delete("/api/notes", (req, res)=>{
-    const params = req.params.id
-    dbUpdate(params, notes);
-    res.redirect('');
-});
-
-module.exports=router
+module.exports=router;
